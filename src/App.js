0e1500eddbf7db79178b69ee0f0e0c3fe29724aa
Jonathan Satoru Toyoshima <jonathan.toyoshima@hogarthww.com>
2019-02-06 19:01:43 -0200

    update new features

--- src/App.js
+++ src/App.js
@@ -1,80 +1,76 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+class Pawn extends Component {
+  constructor(props){
+    super(props);
+    this.pos = this.props.value;
+  }
+  selectPiece(){
+    //Square.setState({ piece: null });
+    //console.log(this.pos);
+    return "hi";
+  }
+
+  render(){
+    return <div className="Pawn"></div>
+  }
+}
+
 class Square extends Component {
+  constructor(props){
+    super(props);
+    this.state = {piece: this.props.piece};
+    this.filled = false;
+  }
+  fn(){
+    //console.log(this.props.value);
+    console.log(this.state.piece);
+    return this.filled = !this.filled;
+  }
   render() {
-    return (<div className="Square"></div>);
+    return (
+      <div className="Square" onClick={()=>this.fn()}>
+        { this.state.piece }
+      </div>);
   };
 }
+
 class Board extends Component {
+  constructor(props){
+    super(props)
+    this.collumns = 3;
+    this.rows = 3;
+    this.Pawn = null;
+    this.list = [1,2,3];
+  }
+  NumberList(){
+    const numbers = ["A1", "A2", "A3"];
+    const doubled = numbers.map((number, index) => this.renderSquare(number,index));
+    return doubled;
+  }
+  renderSquare(number, index){
+    this.Pawn = number == "A1" ? <Pawn /> : null;
+    return <Square key={index} value={number} piece={this.Pawn}/>;
+  }
+
   render () {
     return(
       <div className="Board">
-      <div class="Row">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
+        {this.NumberList()}
       </div>
-      <div class="Row">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      </div>
-      <div class="Row">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      </div>
-      <div class="Row">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      </div>
-    </div>)
+    )
   }
 }
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="App-header"></header>
         <Board />
-
       </div>
     );
   }
 }
-
-
 
 export default App;
