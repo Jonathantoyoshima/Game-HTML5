7cc6879e3dbba391918c59585fbc4a934bedd4e6
Satoru <jonathantoyoshima@hotmail.com>
2019-02-07 10:05:10 -0200

    up

--- src/App.js
+++ src/App.js
@@ -2,23 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Pawn extends Component {
-  constructor(props){
-    super(props);
-    this.pos = this.props.value;
-  }
-  selectPiece(){
-    //Square.setState({ piece: null });
-    //console.log(this.pos);
-    return "hi";
-  }
-
   render(){
     return <div className="Pawn"></div>
   }
 }
 
 class Square extends Component {
-<<<<<<< HEAD
   constructor(props){
     super(props);
     this.state = {piece: this.props.piece};
@@ -26,7 +15,7 @@ class Square extends Component {
   }
   fn(){
     //console.log(this.props.value);
-    console.log(this.state.piece);
+    this.state.piece;
     return this.filled = !this.filled;
   }
   render() {
@@ -34,67 +23,34 @@ class Square extends Component {
       <div className="Square" onClick={()=>this.fn()}>
         { this.state.piece }
       </div>);
-=======
-  constructor(props) {
-    super(props);
-  };
-  render() {
-    return (
-        <div className="Square">
-          {this.props.name}
-      </div>
-    );
->>>>>>> b66c4fd930626f56785b17618c75df4f7fffa432
   };
 }
 
 class Board extends Component {
-<<<<<<< HEAD
   constructor(props){
     super(props)
     this.collumns = 3;
     this.rows = 3;
-    this.Pawn = null;
-    this.list = [1,2,3];
+    this.list = ["A1","A2","A3", "A4"];
+  }
+  handleBoard(number,index){
+    var oPawn = number === "A1" ? <Pawn /> : null;
+    return this.renderSquare(number,index, oPawn);
   }
   NumberList(){
-    const numbers = ["A1", "A2", "A3"];
-    const doubled = numbers.map((number, index) => this.renderSquare(number,index));
+    const doubled = this.list.map((number, index) => this.handleBoard(number,index));
     return doubled;
   }
-  renderSquare(number, index){
-    this.Pawn = number == "A1" ? <Pawn /> : null;
-    return <Square key={index} value={number} piece={this.Pawn}/>;
-=======
-  renderSquare(i) {
-    return <Square value={i} />
->>>>>>> b66c4fd930626f56785b17618c75df4f7fffa432
+  renderSquare(number, index, piece){
+    return <Square key={index} value={number} piece={piece}/>;
   }
 
   render () {
     return(
       <div className="Board">
-<<<<<<< HEAD
         {this.NumberList()}
       </div>
     )
-=======
-      <div class="Row">
-        {this.renderSquare([0,0])}
-        {this.renderSquare([1,0])}
-        {this.renderSquare([2,0])}
-        {this.renderSquare([3,0])}
-        {this.renderSquare([4,0])}
-      </div>
-      <div class="Row">
-        {this.renderSquare([0,1])}
-        {this.renderSquare([1,1])}
-        {this.renderSquare([2,1])}
-        {this.renderSquare([3,1])}
-        {this.renderSquare([4,1])}
-      </div>      
-    </div>)
->>>>>>> b66c4fd930626f56785b17618c75df4f7fffa432
   }
 }
 class App extends Component {
